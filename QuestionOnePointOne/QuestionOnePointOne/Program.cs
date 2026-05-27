00d45e8bb16d21ab24a31c052e90871f0992a1f6
JoshJvR <joshjvr113@gmail.com>
2026-05-27 10:25:49 +0200

    Added unfinished Q1.1
    I have added my almost finished question 1.1 from my Summative Assessment

---
+++ QuestionOnePointOne/QuestionOnePointOne/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Welcome to the Structure of a C# Program Demo!");
+Console.Write("Enter Your Name: ");
+String? Name= Console.ReadLine();
+Console.Write("Hello " );
+Console.Write(Name);
+Console.WriteLine("!");
+Console.WriteLine();
+Console.WriteLine("Program Structure Demonstrated");
+Console.WriteLine("1. using system : imports functionality");
+Console.WriteLine("2. Namespace : Organizes Code");
+Console.WriteLine("3. class program : container for code");
+Console.WriteLine("4. Main() : entry point of program");
+Console.WriteLine("5. comments : explain logic and documentation");
+Console.WriteLine();
+Console.WriteLine("Program executed successfully!");
