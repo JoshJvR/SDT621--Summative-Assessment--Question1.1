22d00284f18a94b898ce142320e2b81ed07987a7
joshj <joshj@JOSHJVR>
2026-05-27 10:34:27 +0200

    I had finished off my Question 1.1 by adding some missing symbols and added some spacing by line 8 and line 16 so that the sections are visibly broken up

--- QuestionOnePointOne/QuestionOnePointOne/Program.cs
+++ QuestionOnePointOne/QuestionOnePointOne/Program.cs
@@ -5,6 +5,7 @@ Console.Write("Hello " );
 Console.Write(Name);
 Console.WriteLine("!");
 Console.WriteLine();
+
 Console.WriteLine("Program Structure Demonstrated");
 Console.WriteLine("1. using system : imports functionality");
 Console.WriteLine("2. Namespace : Organizes Code");
@@ -12,4 +13,5 @@ Console.WriteLine("3. class program : container for code");
 Console.WriteLine("4. Main() : entry point of program");
 Console.WriteLine("5. comments : explain logic and documentation");
 Console.WriteLine();
+
 Console.WriteLine("Program executed successfully!");
